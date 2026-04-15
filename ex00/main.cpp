baf9d9fbbc0b7ae0aa1e3f3031d32ad528591a9b
Jack Mcgrane <jmcgrane@f1r5s15.codam.nl>
2026-04-15 12:13:42 +0200

    final ex01

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmcgrane <jmcgrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 12:56:34 by jmcgrane          #+#    #+#             */
-/*   Updated: 2026/03/31 15:14:52 by jmcgrane         ###   ########.fr       */
+/*   Updated: 2026/04/15 12:10:42 by jmcgrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int main(void)
 	Zombie* z;
 	z = newZombie("Steve");
 	z->announce();
-	delete z;
 	randomChump("Greg");
+	delete z;
 }
