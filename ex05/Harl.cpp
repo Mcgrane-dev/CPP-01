33230d3fb1f3bd43c84596fa89f94b3180e9c52b
Jack Mcgrane <jmcgrane@f1r1s19.codam.nl>
2026-05-01 15:51:52 +0200

    ex05 start

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,33 +6,33 @@
 /*   By: jmcgrane <jmcgrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 14:06:30 by jmcgrane          #+#    #+#             */
-/*   Updated: 2026/05/01 15:44:19 by jmcgrane         ###   ########.fr       */
+/*   Updated: 2026/05/01 15:51:36 by jmcgrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-void debug(void)
+void Harl::debug(void)
 {
 	std::cout << "DBEUG: \n";
 }
 
-void info(void)
+void Harl::info(void)
 {
 	std::cout << "INFO: \n";
 }
 
-void warning(void)
+void Harl::warning(void)
 {
 	std::cout << "WARNING: \n";
 }
 
-void error(void)
+void Harl::error(void)
 {
 	std::cout << "ERROR: \n";
 }
 
-void complain(std::string level)
+void Harl::complain(std::string level)
 {
 	
 }
